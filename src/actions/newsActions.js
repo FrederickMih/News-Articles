100f80584f1ae60d655ef5ca10e9edb2c0c2b2ff
FrederickMih <fredtimermih@gmail.com>
2021-09-03 01:47:58 +0100

    Modify NewsAction file

--- src/actions/newsActions.js
+++ src/actions/newsActions.js
@@ -1,4 +1,7 @@
 export const FETCH_CUSTOM_NEWS = 'FETCH_CUSTOM_NEWS';
+export const NEWS_ARTICLE_ERROR = 'NEWS_ARTICLE_ERROR';
+export const CHANGE_FILTER = 'CHANGE_FILTER';
+
 export const fetchCustomNews = (source, mostImportant) => (dispatch) => {
   const apiKey = '08dd40237d8148d9ade088ed2744791c';
   fetch(`https://newsapi.org/v2/everything?sources=${source}&sortBy=${mostImportant}&apiKey=${apiKey}`)
@@ -14,3 +17,13 @@ export const fetchCustomNews = (source, mostImportant) => (dispatch) => {
       console.log(err);
     });
 };
+
+export const newsArticleError = (error) => ({
+  type: NEWS_ARTICLE_ERROR,
+  payload: { error },
+});
+
+export const changeFilter = (filter) => ({
+  type: CHANGE_FILTER,
+  payload: filter,
+});
