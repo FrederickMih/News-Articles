0ee78dbf6f378ab03df4f4945c5c1bf61a3884f9
FrederickMih <fredtimermih@gmail.com>
2021-09-04 21:47:27 +0100

    Add some Styles for the Navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import Routes from '../routes/routes';
+import Navbar from './Navbar';
 
 const App = () => (
   <div className="app-div">
+    <Navbar />
     <Routes />
   </div>
 );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import '../styles/Navbar.css';
+import '../Styles/Navbar.css';
 
 const Navbar = () => (
   <nav className="navbar">
