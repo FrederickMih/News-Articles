d4ba3ccf2017f1e06e162a82ee3b58ec4eb2323c
FrederickMih <fredtimermih@gmail.com>
2021-09-04 21:53:00 +0100

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="theFooter">
+    Copyright
+    {' '}
+    &copy;
+    {' '}
+    <span>Mih Frederick</span>
+  </footer>
+);
+
+export default Footer;
